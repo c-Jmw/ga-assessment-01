c80a86a60262e10326a48ce930ae2a46b8f8af98
Casey <casey.j.wilkinson@gmail.com>
2015-04-21 18:40:01 +1000

    Initial commit of project files
    same as above

---
+++ assessment1/assessment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assessment1
+//
+//  Created by Casey Macrae on 21/04/2015.
+//  Copyright (c) 2015 Casey Macrae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
